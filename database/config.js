e5f5faa19470aa02ced001064307678b778180dc
Andres Hidalgo <andres_fel.hidalgo@uao.edu.co>
2023-05-27 11:59:52 -0500

    correcion dbConnection

--- database/config.js
+++ database/config.js
@@ -4,8 +4,8 @@ const dbConnection = async () => {
     try{
         mongoose.connect(process.env.DB_CONNECTION, {
             autoIndex: true
-        })
-        console.log('DB online')
+        }).then(() => {console.log('DB online')})
+        
     }catch(error){
         console.log(error)
         throw new Error('Erro al conectar a la base de datos')
